bf31884a58b2845315ad01a8b46e48eda3291ca4
petiteZel <2882loverjqnrdl@gmail.com>
2024-01-08 06:26:49 +0900

    feat: footer 생성

--- footer.php
+++ footer.php
@@ -6,7 +6,16 @@ Version: 1.0
 */
 
 ?>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<section id="footer">
+    <!--  -->
+    <p>(C) 2024. 최승연. All rights reserved.</p>
+    <p>이 홈페이지는 워드프레스 테마를 직접 개발하여 제작되었습니다.</p>
+    <div class="contact">
+        <span><i class="fas fa-phone"></i> &nbsp; 010-4931-0660</span>
+        <span><i class="fas fa-envelope"></i> &nbsp; csykik@naver.com</span>
+    </div>
+</section>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
